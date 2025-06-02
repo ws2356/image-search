12e186b4f0324fe199a4270caea98ac485665272
ws2356 <wansong2356@hotmail.com>
2025-06-03 01:47:47 +0800

    structure the code

--- openclip-main/main.py
+++ openclip-main/main.py
@@ -1,6 +1,6 @@
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow
-from ui_main import Ui_MainWindow
+from ui.ui_main import Ui_MainWindow
 
 class MainWindow(QMainWindow):
     def __init__(self):
