d4a5c48dfd95c2f867af4ac20c904bef515fdb3b
ws2356 <wansong2356@hotmail.com>
2025-06-08 00:54:27 +0800

    relayout when app window size changes

--- desktop/main.py
+++ desktop/main.py
@@ -2,7 +2,7 @@ import os
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog, QLabel, QWidget, QGridLayout
 from PySide6.QtGui import QPixmap
-from PySide6.QtCore import Qt
+from PySide6.QtCore import Qt, QEvent
 
 from ui.mainwindow_ui import Ui_MainWindow
 
@@ -23,7 +23,12 @@ class MainWindow(QMainWindow):
         self.ui.addFolderButton.clicked.connect(self.add_folder)
         self.ui.folderList.currentRowChanged.connect(self.display_folder_images)
         self.ui.searchInput.textChanged.connect(self.handle_search)
+        self.folder = None
 
+    def resizeEvent(self, event: QEvent):
+        super().resizeEvent(event)  # preserve base behavior
+        if self.folder:
+            self.load_images_from_folder(self.folder)
 
     def add_folder(self):
         folder = QFileDialog.getExistingDirectory(self, "Select Folder")
@@ -35,8 +40,7 @@ class MainWindow(QMainWindow):
     def display_folder_images(self, index):
         if index < 0 or index >= len(self.folder_paths):
             return
-        folder = self.folder_paths[index]
-        self.load_images_from_folder(folder)
+        self.load_images_from_folder(self.folder_paths[index])
 
     def load_images_from_folder(self, folder):
         # Clear old thumbnails
@@ -45,24 +49,31 @@ class MainWindow(QMainWindow):
             if widget:
                 widget.setParent(None)
 
+        self.folder = folder
         image_exts = (".png", ".jpg", ".jpeg", ".bmp", ".gif")
         files = [f for f in os.listdir(folder) if f.lower().endswith(image_exts)]
+
+        cell_width = 150
+        scroll_area_width = self.ui.imageScrollArea.viewport().width()
+        num_columns = max(1, scroll_area_width // cell_width)
 
         row = col = 0
         for file in files:
             path = os.path.join(folder, file)
-            pixmap = QPixmap(path).scaled(150, 150, Qt.KeepAspectRatio, Qt.SmoothTransformation)
+            pixmap = QPixmap(path).scaled(cell_width, cell_width, Qt.KeepAspectRatio, Qt.SmoothTransformation)
             label = QLabel()
             label.setPixmap(pixmap)
+            label.setFixedSize(cell_width, cell_width)
+            label.setAlignment(Qt.AlignCenter)
+
             self.image_grid_layout.addWidget(label, row, col)
             col += 1
-            if col >= 4:
+            if col >= num_columns:
                 col = 0
                 row += 1
 
     def handle_search(self, query):
         pass
-
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
