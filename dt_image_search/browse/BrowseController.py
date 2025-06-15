3d70fb5def19fca062235318e25489a82cc1963b
ws2356 <wansong2356@hotmail.com>
2025-06-16 00:24:35 +0800

    persistence for browse

--- dt_image_search/browse/BrowseController.py
+++ dt_image_search/browse/BrowseController.py
@@ -3,6 +3,7 @@ from dt_image_search.base.BaseController import BaseController
 from PySide6.QtCore import QAbstractListModel, Qt, QModelIndex, QThreadPool, QSize
 from dt_image_search.browse.image_list_model import ImageListModel
 from dt_image_search.browse.folder_list_model import FolderListModel
+from dt_image_search.model.db import create_db_conn, insert_folder, get_all_folders, get_files_for_folder
 
 class BrowseController(BaseController):
     def __init__(self):
@@ -25,6 +26,9 @@ class BrowseController(BaseController):
             logging.info(f"on_folder_added: {folder_path}")
             self.folderListModel.folders.append(folder_path)
             self.folderListModel.layoutChanged.emit()
+            with create_db_conn() as conn:
+                folder_id = insert_folder(conn, folder_path)
+                logging.info(f"Inserted folder with ID: {folder_id}")
 
     def on_folder_selected(self, row: int):
         folder_path = self.folder_list_model().folders[row]
