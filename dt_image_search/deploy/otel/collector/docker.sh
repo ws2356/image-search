a0844a471d6b1edc97c136d41f54bd41f52d8b4c
My name <My email>
2025-07-08 23:45:32 +0800

    update deploy files

--- dt_image_search/deploy/otel/collector/docker.sh
+++ dt_image_search/deploy/otel/collector/docker.sh
@@ -1,3 +1,3 @@
-docker run --rm -p 4318:4318 -p 8889:8889 \
+docker run --rm -p 4318:4318 -p 8889:8889 --network 'sa-net' \
   -v "$(pwd)/otel-collector-config.yaml":/etc/otelcol/config.yaml \
   otel/opentelemetry-collector:latest
---
+++ dt_image_search/deploy/otel/grafana/docker.sh
@@ -0,0 +1,1 @@
+docker run --rm -p 3000:3000 grafana/grafana:w
