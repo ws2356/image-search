3bf9167db9a77e2298c94b5469bc9b0c694f9404
My name <My email>
2025-07-12 16:55:20 +0800

    opentelemetry setup

--- dt_image_search/deploy/otel/grafana/docker.sh
+++
@@ -1,1 +0,0 @@
-docker run --rm -p 3000:3000 grafana/grafana:w
--- dt_image_search/deploy/otel/prometheus/docker.sh
+++
@@ -1,3 +0,0 @@
-docker run --rm -p 9090:9090 --network 'sa-net' \
-  -v "$(pwd)/prometheus.yml":/etc/prometheus/prometheus.yml \
-  prom/prometheus
