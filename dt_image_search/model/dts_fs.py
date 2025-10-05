15b8debc8182b0ef852f3d617417ec0b17473f07
ws2356 <wansong2356@hotmail.com>
2025-10-05 14:47:00 +0800

    try adding a lock around data path init

--- dt_image_search/model/dts_fs.py
+++ dt_image_search/model/dts_fs.py
@@ -1,17 +1,19 @@
 from pathlib import Path
+import threading
 from PySide6.QtCore import QStandardPaths
 
 _data_path = None
+_data_lock = threading.Lock()
 
 def get_app_data_path() -> Path:
     global _data_path
-    if _data_path is not None:
-        return _data_path
-    APP_NAME = "DTImageSearch"
-    base_path = QStandardPaths.writableLocation(QStandardPaths.AppDataLocation)
-    data_path = Path(base_path) / APP_NAME
-    data_path.mkdir(parents=True, exist_ok=True)
-    _data_path = data_path
+    # Add a lock to protect against reentrant calls
+    with _data_lock:
+        if _data_path is None:
+            APP_NAME = "DTImageSearch"
+            base_path = QStandardPaths.writableLocation(QStandardPaths.AppLocalDataLocation)
+            _data_path = Path(base_path) / APP_NAME
+            _data_path.mkdir(parents=True, exist_ok=True)
     return _data_path
 
 def get_pretrained_model_cache_path() -> Path:
