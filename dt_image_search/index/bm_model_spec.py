d3d99f30009e66c14fc0e324149fb2e5866849da
My name <My email>
2025-10-12 00:41:02 +0800

    Update bm_model_spec.py

--- dt_image_search/index/bm_model_spec.py
+++ dt_image_search/index/bm_model_spec.py
@@ -1,4 +1,5 @@
 model_name = "ViT-B-32"
-# model_name = "xlm-roberta-base-ViT-B-32"
+model_name2 = "xlm-roberta-base-ViT-B-32"
+
 pretrained_model = "laion2b_s34b_b79k"
-# pretrained_model = "laion5b_s13b_b90k"
+pretrained_model2 = "laion5b_s13b_b90k"
