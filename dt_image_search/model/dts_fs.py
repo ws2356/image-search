0d83cdbaa8949ebcceafbaf5a247cea3251f508e
My name <My email>
2025-10-12 02:07:15 +0800

    Fix wrong data path being cached

--- dt_image_search/model/dts_fs.py
+++ dt_image_search/model/dts_fs.py
@@ -8,13 +8,14 @@ _data_lock = threading.Lock()
 
 def get_app_data_path(ctx: BMContext) -> Path:
     # Add a lock to protect against reentrant calls
+    data_path_cache_key = f"BM_DATA_PATH_{ctx.subfolder}"
     with _data_lock:
-        if not os.getenv("BM_DATA_PATH"):
+        if not os.getenv(data_path_cache_key):
             APP_NAME = "DTImageSearch"
             _base_path = QStandardPaths.writableLocation(QStandardPaths.AppLocalDataLocation)
             _data_path = Path(_base_path) / APP_NAME
             if ctx.subfolder:
                 _data_path = _data_path / ctx.subfolder
             _data_path.mkdir(parents=True, exist_ok=True)
-            os.environ["BM_DATA_PATH"] = str(_data_path)
-    return Path(os.getenv("BM_DATA_PATH"))
+            os.environ[data_path_cache_key] = str(_data_path)
+    return Path(os.getenv(data_path_cache_key))
