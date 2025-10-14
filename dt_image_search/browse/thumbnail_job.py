ea8bc33ea8dcdf4e93621c0ce594277922c03e52
My name <My email>
2025-10-14 23:13:59 +0800

    1. catch render error 2. show indexing progress in  status bar

--- dt_image_search/browse/thumbnail_job.py
+++ dt_image_search/browse/thumbnail_job.py
@@ -19,15 +19,20 @@ class ThumbnailJob(QRunnable):
 
     # @with_trace("ThumbnailJob.run")
     def run(self):
-        pil_image = Image.open(self.path).convert("RGBA")
-        data = pil_image.tobytes("raw", "RGBA")
-        image = QImage(data, pil_image.width, pil_image.height, QImage.Format_RGBA8888)
-        image = image.scaled(
-            self.icon_size.width(), self.icon_size.height(),
-            Qt.KeepAspectRatio, Qt.SmoothTransformation
-        )
-        if not image or image.isNull():
-            log("error", "image_thumbnail", f"Failed to create thumbnail for {self.path}", __file__)
-        thread = threading.current_thread()
-        log("debug", message= f"Thumbnail created [{thread.name} - {thread.ident}]: {self.path}")
-        self.signals.finished.emit(self.path, image)
+        image = None
+        try:
+            pil_image = Image.open(self.path).convert("RGBA")
+            data = pil_image.tobytes("raw", "RGBA")
+            image = QImage(data, pil_image.width, pil_image.height, QImage.Format_RGBA8888)
+            image = image.scaled(
+                self.icon_size.width(), self.icon_size.height(),
+                Qt.KeepAspectRatio, Qt.SmoothTransformation
+            )
+            if not image or image.isNull():
+                log("error", "image_thumbnail", f"Failed to create thumbnail for {self.path}", __file__)
+            thread = threading.current_thread()
+            log("debug", message= f"Thumbnail created [{thread.name} - {thread.ident}]: {self.path}")
+        except Exception as e:
+            log("error", "image_thumbnail", f"Error creating thumbnail for {self.path}: {e}", __file__)
+        finally:
+            self.signals.finished.emit(self.path, image)
