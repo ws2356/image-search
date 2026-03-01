73326f6b3b936dfb89b1439dec0753369fb7609a
ws2356 <wansong2356@hotmail.com>
2026-03-01 15:29:03 +0800

    trivial

--- dt_image_search/scripts/run_tests.sh
+++ dt_image_search/scripts/run_tests.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
+
+level="${1:-INFO}"
+
 export PYTHONPATH=$PYTHONPATH:.
 
 # python3 -m pytest tests/unit/test_dts_db.py
-python3 -m pytest -s --log-cli-level=INFO tests/unit/test_dts_index.py
-python3 -m pytest -s --log-cli-level=INFO tests/unit/test_search_controller.py
-python3 -m pytest -s --log-cli-level=INFO tests/functional/test_app_flow.py
+python3 -m pytest -s --log-cli-level=$level tests/unit/test_dts_index.py
+python3 -m pytest -s --log-cli-level=$level tests/unit/test_search_controller.py
+python3 -m pytest -s --log-cli-level=$level tests/functional/test_app_flow.py
