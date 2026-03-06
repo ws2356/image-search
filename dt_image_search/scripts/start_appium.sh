3e3ca9561e90f13b08bc63f5b2ed2c7b555c6848
ws2356 <wansong2356@hotmail.com>
2026-03-06 15:12:32 +0800

    Update start_appium

--- dt_image_search/scripts/start_appium.sh
+++ dt_image_search/scripts/start_appium.sh
@@ -5,4 +5,15 @@ set -euo pipefail
 lsof -i :4723 -t | xargs -r kill -SIGTERM || true
 lsof -i :10100 -t | xargs -r kill -SIGTERM || true
 
+# If macos
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # Check if mac2 driver is installed
+  if ! npm list -g appium-mac2-driver > /dev/null 2>&1; then
+    echo "Appium Mac2 driver is not installed. Installing..."
+    npm install -g appium-mac2-driver
+  fi
+else
+  node_modules/.bin/appium driver install windows
+fi
+
 node_modules/.bin/appium --use-plugins=inspector --allow-cors  --relaxed-security || true
