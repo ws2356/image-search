3cf4516d026d26d68ffa80b3bcfbb69e48db5073
ws2356 <wansong2356@hotmail.com>
2026-03-29 21:20:41 +0800

    fix test scripts

--- dt_image_search/scripts/run_tests.sh
+++ dt_image_search/scripts/run_tests.sh
@@ -2,6 +2,12 @@
 
 set -euo pipefail
 
+this_scrit="$0"
+if [[ "$this_scrit" != /* ]]; then
+  this_scrit="$(pwd)/$this_scrit"
+fi
+this_dir="$(dirname "$this_scrit")"
+
 level=INFO
 need_build=false
 
@@ -27,18 +33,25 @@ export PYTHONPATH=${PYTHONPATH:-}${PYTHONPATH:+:}.
 
 echo "PYTHONPATH set to: $PYTHONPATH"
 
-# python3 -m pytest tests/unit/test_dts_db.py
-# python3 -m pytest -s --log-cli-level=$level tests/unit/test_dts_index.py
-# python3 -m pytest -s --log-cli-level=$level tests/unit/test_search_controller.py
-# python3 -m pytest -s --log-cli-level=$level tests/functional/test_app_flow.py
+python_bin=python3
+if ! command -v $python_bin &> /dev/null; then
+    echo "$python_bin could not be found, trying python..."
+    python_bin=python
+    if ! command -v $python_bin &> /dev/null; then
+        echo "Neither python nor python3 could be found. Please install Python and ensure it's in your PATH."
+        exit 1
+    fi
+fi
+
+$python_bin -m pytest tests/unit/test_dts_db.py
+$python_bin -m pytest -s --log-cli-level=$level tests/unit/test_dts_index.py
+$python_bin -m pytest -s --log-cli-level=$level tests/unit/test_search_controller.py
+$python_bin -m pytest -s --log-cli-level=$level tests/functional/test_app_flow.py
 
 if [ "$need_build" = true ]; then
     echo "Building app with PyInstaller..."
     pyinstaller dt_image_search/DTImageSearch.spec --noconfirm --clean
 fi
 
-if type python >/dev/null ; then
-  python tests/integration/test_golden_path_appium.py
-else
-  python3 tests/integration/test_golden_path_appium.py
-fi
+bash "$this_dir/uitest_bootstrap.sh"
+bash "$this_dir/uitest_start.sh"
