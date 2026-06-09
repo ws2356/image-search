cb34fa515031dfe4bd2153c6b0dbbfcc4899766a
ws2356 <wansong2356@hotmail.com>
2026-06-09 21:31:24 +0800

    [fix] swiftui bug

--- mobile/ios/Sources/ISFromPC/Views/RichTextReceiveView.swift
+++ mobile/ios/Sources/ISFromPC/Views/RichTextReceiveView.swift
@@ -54,6 +54,14 @@ public struct RichTextReceiveView: View {
 
 struct RichTextWebView: UIViewRepresentable {
     let html: String
+
+    class Coordinator {
+        var lastHTML: String?
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator()
+    }
     
     func makeUIView(context: Context) -> WKWebView {
         let config = WKWebViewConfiguration()
@@ -61,10 +69,17 @@ struct RichTextWebView: UIViewRepresentable {
         let webView = WKWebView(frame: .zero, configuration: config)
         webView.scrollView.isScrollEnabled = true
         webView.loadHTMLString(html, baseURL: nil)
+        context.coordinator.lastHTML = html
         return webView
     }
     
-    func updateUIView(_ uiView: WKWebView, context: Context) {}
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        guard html != context.coordinator.lastHTML else {
+            return
+        }
+        context.coordinator.lastHTML = html
+        uiView.loadHTMLString(html, baseURL: nil)
+    }
 }
 
 struct ToastView: View {
