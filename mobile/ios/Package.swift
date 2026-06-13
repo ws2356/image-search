2b2b3d77bc2ff71097dd85100442da0fc24c524e
ws2356 <wansong2356@hotmail.com>
2026-06-14 00:18:37 +0800

    [feat] iOS create cert using swift-certificate

--- mobile/ios/Package.swift
+++ mobile/ios/Package.swift
@@ -28,6 +28,8 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/hmlongco/Factory.git", from: "2.4.12"),
         .package(url: "https://github.com/open-telemetry/opentelemetry-swift-core.git", from: "2.3.0"),
+        .package(url: "https://github.com/apple/swift-asn1.git", from: "1.3.0"),
+        .package(url: "https://github.com/apple/swift-certificates.git", from: "1.0.0"),
     ],
     targets: [
         .target(
@@ -47,6 +49,8 @@ let package = Package(
                 .product(name: "Factory", package: "Factory"),
                 .product(name: "OpenTelemetryApi", package: "opentelemetry-swift-core"),
                 .product(name: "OpenTelemetrySdk", package: "opentelemetry-swift-core"),
+                .product(name: "SwiftASN1", package: "swift-asn1"),
+                .product(name: "X509", package: "swift-certificates"),
                ],
                resources: []),
         .target(name: "ISFromPC",
--- mobile/ios/Sources/Common/Services/KeychainAppIdentityProvider.swift
+++ mobile/ios/Sources/Common/Services/KeychainAppIdentityProvider.swift
@@ -1,6 +1,8 @@
 import Foundation
 import Security
 import CryptoKit
+import X509
+import SwiftASN1
 
 public protocol AppIdentityProviding: Sendable {
     func ensureSelfIdentity() async throws
@@ -204,115 +206,39 @@ public final class KeychainAppIdentityProvider: AppIdentityProviding {
     // MARK: - Certificate
 
     private func buildSelfSignedCert(privateKey: SecKey, rawPubKey: Data, commonName: String) throws -> SecCertificate {
-        let subject = _DistinguishedName(commonName: commonName)
-        let notBefore = Date()
-        let notAfter = Calendar.current.date(byAdding: .year, value: 10, to: notBefore)!
-        let serial = Data((0..<16).map { _ in UInt8.random(in: 0...255) })
-        let spki = _encodeSubjectPublicKeyInfo(rawPubKey)
-        let tbs = _encTBSCert(serial: serial, issuer: subject, nb: notBefore, na: notAfter, subj: subject, spki: spki)
-        let sigAlgo = _encSigAlgo()
-        let digest = SHA256.hash(data: tbs)
-        guard let signature = SecKeyCreateSignature(privateKey, .ecdsaSignatureDigestX962SHA256, Data(digest) as CFData, nil) as Data? else {
+        let p256PubKey = try P256.Signing.PublicKey(x963Representation: rawPubKey)
+
+        let serialNumber = Certificate.SerialNumber()
+        let dn = try DistinguishedName { CommonName(commonName) }
+        let now = Date().addingTimeInterval(-3600.0 * 24.0)
+        let notAfter = Calendar.current.date(byAdding: .day, value: 364, to: now)!
+
+        let extensions = try Certificate.Extensions {
+            BasicConstraints.isCertificateAuthority(maxPathLength: nil)
+            try ExtendedKeyUsage([.clientAuth])
+        }
+
+        let certificate = try Certificate(
+            version: .v3,
+            serialNumber: serialNumber,
+            publicKey: Certificate.PublicKey(p256PubKey),
+            notValidBefore: now,
+            notValidAfter: notAfter,
+            issuer: dn,
+            subject: dn,
+            signatureAlgorithm: .ecdsaWithSHA256,
+            extensions: extensions,
+            issuerPrivateKey: try Certificate.PrivateKey(privateKey)
+        )
+
+        var serializer = DER.Serializer()
+        try serializer.serialize(certificate)
+        let derData = Data(serializer.serializedBytes)
+
+        guard let secCert = SecCertificateCreateWithData(nil, derData as CFData) else {
             throw IdentityError.certificateCreationFailed
         }
-        let sigVal = _tlv(0x03, Data([0x00]) + signature)
-        let derCert = _tlv(0x30, tbs + sigAlgo + sigVal)
-        guard let cert = SecCertificateCreateWithData(nil, derCert as CFData) else {
-            throw IdentityError.certificateCreationFailed
-        }
-        return cert
-    }
-
-    // MARK: - ASN.1 helpers
-
-    private func _tlv(_ tag: UInt8, _ value: Data) -> Data {
-        _encodeASN1(tag: tag, value: value)
-    }
-
-    private func _encodeASN1(tag: UInt8, value: Data) -> Data {
-        var result = Data([tag])
-        if value.count < 128 {
-            result.append(UInt8(value.count))
-        } else {
-            var lenBytes = Data()
-            var rem = value.count
-            while rem > 0 {
-                lenBytes.insert(UInt8(rem & 0xFF), at: 0)
-                rem >>= 8
-            }
-            result.append(UInt8(0x80 | lenBytes.count))
-            result.append(lenBytes)
-        }
-        result.append(value)
-        return result
-    }
-
-    private func _encodeSubjectPublicKeyInfo(_ rawPublicKey: Data) -> Data {
-        let ecPubOID = Data([0x06, 0x07, 0x2A, 0x86, 0x48, 0xCE, 0x3D, 0x02, 0x01])
-        let p256OID = Data([0x06, 0x08, 0x2A, 0x86, 0x48, 0xCE, 0x3D, 0x03, 0x01, 0x07])
-        let algoParams = _tlv(0x30, ecPubOID + p256OID)
-        let pubKey = _tlv(0x03, Data([0x00]) + rawPublicKey)
-        return _tlv(0x30, algoParams + pubKey)
-    }
-
-    private func _encTBSCert(serial: Data, issuer: _DistinguishedName, nb: Date, na: Date, subj: _DistinguishedName, spki: Data) -> Data {
-        var inner = Data()
-        inner += _encInt(serial)
-        inner += _encSigAlgo()
-        inner += issuer._encode()
-        inner += _tlv(0x30, _encTime(nb) + _encTime(na))
-        inner += subj._encode()
-        inner += spki
-        return _tlv(0x30, inner)
-    }
-
-    private func _encSigAlgo() -> Data {
-        _tlv(0x30, Data([0x06, 0x08, 0x2A, 0x86, 0x48, 0xCE, 0x3D, 0x04, 0x03, 0x02]))
-    }
-
-    private func _encInt(_ v: Data) -> Data {
-        var x = v
-        if !x.isEmpty, x[0] & 0x80 != 0 { x.insert(0, at: 0) }
-        return _tlv(0x02, x)
-    }
-
-    private func _encTime(_ d: Date) -> Data {
-        let f = DateFormatter()
-        f.timeZone = TimeZone(identifier: "UTC")
-        f.dateFormat = "yyMMddHHmmss'Z'"
-        return _tlv(0x17, Data(f.string(from: d).utf8))
-    }
-}
-
-// MARK: - DistinguishedName
-
-private struct _DistinguishedName {
-    let commonName: String
-
-    func _encode() -> Data {
-        let cnOID = Data([0x06, 0x03, 0x55, 0x04, 0x03])
-        let cnValue = _encodeASN1(tag: 0x0C, value: Data(commonName.utf8))
-        let cnAttr = _encodeASN1(tag: 0x30, value: cnOID + cnValue)
-        let cnSet = _encodeASN1(tag: 0x31, value: cnAttr)
-        return _encodeASN1(tag: 0x30, value: cnSet)
-    }
-
-    private func _encodeASN1(tag: UInt8, value: Data) -> Data {
-        var result = Data([tag])
-        if value.count < 128 {
-            result.append(UInt8(value.count))
-        } else {
-            var lenBytes = Data()
-            var rem = value.count
-            while rem > 0 {
-                lenBytes.insert(UInt8(rem & 0xFF), at: 0)
-                rem >>= 8
-            }
-            result.append(UInt8(0x80 | lenBytes.count))
-            result.append(lenBytes)
-        }
-        result.append(value)
-        return result
+        return secCert
     }
 }
 
