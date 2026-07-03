95523325a4409ce138166c77fb2fbcee3884e4b6
ws2356 <wansong2356@hotmail.com>
2026-07-03 14:27:24 +0800

    [feat] create mobile/instant-share

---
+++ mobile/instant-share/instant-share/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  instant-share
+//
+//  Created by Song Wan on 7/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
