764c85bc455a617c3645a3e99462f8f538bf25ba
ws2356 <wansong2356@hotmail.com>
2026-07-03 14:45:47 +0800

    refactor: extract InstantShareKit into shared SPM package at mobile/ios-packages/InstantShareKit

---
+++ mobile/ios-packages/InstantShareKit/Package.swift
@@ -0,0 +1,45 @@
+// swift-tools-version: 6.0
+
+import PackageDescription
+
+let package = Package(
+    name: "InstantShareKit",
+    platforms: [
+        .iOS(.v15),
+    ],
+    products: [
+        .library(
+            name: "ISFromPC",
+            type: .dynamic,
+            targets: ["ISFromPC"]
+        ),
+        .library(
+            name: "ISFromMobile",
+            type: .dynamic,
+            targets: ["ISFromMobile"]
+        ),
+    ],
+    dependencies: [
+        .package(path: "../Common"),
+        .package(url: "https://github.com/hmlongco/Factory.git", from: "2.4.12"),
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture.git", from: "1.17.0"),
+    ],
+    targets: [
+        .target(
+            name: "ISFromPC",
+            dependencies: [
+                .product(name: "Common", package: "Common"),
+            ],
+            resources: []
+        ),
+        .target(
+            name: "ISFromMobile",
+            dependencies: [
+                .product(name: "Factory", package: "Factory"),
+                .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
+                .product(name: "Common", package: "Common"),
+            ],
+            resources: []
+        ),
+    ]
+)
