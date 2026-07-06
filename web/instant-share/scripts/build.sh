db814d8e583051e2dd42420630adbaf2691afaa2
ws2356 <wansong2356@hotmail.com>
2026-07-06 23:51:47 +0800

    [feat] polish deploy scripts

--- web/instant-share/scripts/build.sh
+++ web/instant-share/scripts/build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -euo pipefail
-cd "$(dirname "$0")/.."
+
+this_file="${BASH_SOURCE[0]}"
+this_dir="$(cd "$(dirname "$this_file")" && pwd)"
+
+cd "$this_dir/.."
 pnpm install
 pnpm run build
-echo "Built dist/"
+
+cd "$this_dir/../relay"
+pnpm install
