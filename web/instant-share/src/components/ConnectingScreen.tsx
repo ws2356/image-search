43114fb60348bbbab1418983faaaaa41968504e8
ws2356 <wansong2356@hotmail.com>
2026-07-07 22:47:59 +0800

    [fix] connecting screen layout

--- web/instant-share/src/components/ConnectingScreen.tsx
+++ web/instant-share/src/components/ConnectingScreen.tsx
@@ -2,8 +2,10 @@ import { LoadingSpinner } from './ui/ProgressIndicator';
 
 export function ConnectingScreen({ label = 'Connecting to PC…' }: { label?: string }) {
   return (
-    <div className="min-h-screen bg-background">
-      <LoadingSpinner message={label} />
+    <div className="flex min-h-screen flex-col items-center justify-center bg-background">
+      <div className="pb-16">
+        <LoadingSpinner message={label} />
+      </div>
     </div>
   );
 }
