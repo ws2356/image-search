ecada1512a91bb82c0c3ad10722995dd90b1dd4d
ws2356 <wansong2356@hotmail.com>
2026-07-11 01:41:08 +0800

    [LLM: opencode-go/kimi-k2.7-code] Add FileCard dispatcher

---
+++ mobile/ios-packages/InstantShareKit/Sources/ISFromPC/Views/Components/FileCards/FileCard.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+
+#if os(iOS)
+struct FileCard: View {
+    let state: MultiFileReceiveViewModel.FileDownloadState
+    let shareAction: () -> Void
+
+    var body: some View {
+        switch state.entryType.lowercased() {
+        case "text":
+            TextFileCard(state: state, shareAction: shareAction)
+        case "html":
+            HTMLFileCard(state: state, shareAction: shareAction)
+        case "link":
+            WebLinkCard(state: state, shareAction: shareAction)
+        case "file":
+            if state.contentType.lowercased().hasPrefix("image/") {
+                ImageFileCard(state: state, shareAction: shareAction)
+            } else {
+                GenericFileCard(state: state, shareAction: shareAction)
+            }
+        default:
+            GenericFileCard(state: state, shareAction: shareAction)
+        }
+    }
+}
+#endif
