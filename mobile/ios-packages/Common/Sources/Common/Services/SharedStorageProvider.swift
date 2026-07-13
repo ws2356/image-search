605b9b1c62984630f7d4bf550e4dec5b4309cc12
ws2356 <wansong2356@hotmail.com>
2026-07-13 17:14:17 +0800

    feat: add SnapGet app group hasCompletedSession to SharedStorageProvider [LLM: opencode/mimo-v2.5-pro]

--- mobile/ios-packages/Common/Sources/Common/Services/SharedStorageProvider.swift
+++ mobile/ios-packages/Common/Sources/Common/Services/SharedStorageProvider.swift
@@ -9,10 +9,21 @@ import Foundation
 
 public protocol SharedStorageProtocol {
     var commonAppGroupUserDefaults: UserDefaults { get }
+    var snapgetAppGroupUserDefaults: UserDefaults { get }
+    var hasCompletedSession: Bool { get set }
 }
 
 let appGroupIdentifier = "group.net.boldman.common"
+let snapgetAppGroupIdentifier = "group.net.boldman.snapget"
 
 public class SharedStorageProvider: SharedStorageProtocol {
     public let commonAppGroupUserDefaults: UserDefaults = .init(suiteName: appGroupIdentifier)!
+    public let snapgetAppGroupUserDefaults: UserDefaults = .init(suiteName: snapgetAppGroupIdentifier)!
+
+    public var hasCompletedSession: Bool {
+        get { snapgetAppGroupUserDefaults.bool(forKey: "hasCompletedSession") }
+        set { snapgetAppGroupUserDefaults.set(newValue, forKey: "hasCompletedSession") }
+    }
+
+    public init() {}
 }
