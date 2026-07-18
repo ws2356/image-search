fd8a140bfc993b3af2dec5cde9167b03a6d905e6
ws2356 <wansong2356@hotmail.com>
2026-07-18 14:05:33 +0800

    [feat] enable cf proxy (cdn)

--- web/terraform/dns.tf
+++ web/terraform/dns.tf
@@ -32,108 +32,21 @@ resource "cloudflare_dns_record" "wildcard_cname" {
 # A 记录 (指向具体 IP)
 # ==========================================
 
-resource "cloudflare_dns_record" "tc_a" {
-  zone_id = cloudflare_zone.boldman_net.id
-  name    = "tc"
-  type    = "A"
-  content = var.origin_ip
-  ttl     = 3600
-  proxied = false
-}
 
 # ==========================================
 # CNAME 记录 (别名指向)
 # ==========================================
 
-resource "cloudflare_dns_record" "api_cname" {
+resource "cloudflare_dns_record" "www_cname" {
   zone_id = cloudflare_zone.boldman_net.id
-  name    = "api"
+  name    = "www"
   type    = "CNAME"
-  content   = "tc.boldman.net"
-  ttl     = 3600
-  proxied = false
+  content   = "@"
+  ttl     = 1
+  proxied = true
 }
 
-resource "cloudflare_dns_record" "f_cname" {
-  zone_id = cloudflare_zone.boldman_net.id
-  name    = "f"
-  type    = "CNAME"
-  content   = "tc.boldman.net"
-  ttl     = 3600
-  proxied = false
-}
-
-resource "cloudflare_dns_record" "grafana2_cname" {
-  zone_id = cloudflare_zone.boldman_net.id
-  name    = "grafana2"
-  type    = "CNAME"
-  content   = "tc.boldman.net"
-  ttl     = 3600
-  proxied = false
-}
-
-resource "cloudflare_dns_record" "imagesearch_cname" {
-  zone_id = cloudflare_zone.boldman_net.id
-  name    = "imagesearch"
-  type    = "CNAME"
-  content   = "tc.boldman.net"
-  ttl     = 900
-  proxied = false
-}
-
-resource "cloudflare_dns_record" "imagesearch2_cname" {
-  zone_id = cloudflare_zone.boldman_net.id
-  name    = "imagesearch2"
-  type    = "CNAME"
-  content   = "tc.boldman.net"
-  ttl     = 3600
-  proxied = false
-}
-
-resource "cloudflare_dns_record" "otel_cname" {
-  zone_id = cloudflare_zone.boldman_net.id
-  name    = "otel"
-  type    = "CNAME"
-  content   = "tc.boldman.net"
-  ttl     = 900
-  proxied = false
-}
-
-resource "cloudflare_dns_record" "otel2_cname" {
-  zone_id = cloudflare_zone.boldman_net.id
-  name    = "otel2"
-  type    = "CNAME"
-  content   = "tc.boldman.net"
-  ttl     = 3600
-  proxied = false
-}
-
-resource "cloudflare_dns_record" "s_cname" {
-  zone_id = cloudflare_zone.boldman_net.id
-  name    = "s"
-  type    = "CNAME"
-  content   = "tc.boldman.net"
-  ttl     = 3600
-  proxied = false
-}
-
-resource "cloudflare_dns_record" "dl_cname" {
-  zone_id = cloudflare_zone.boldman_net.id
-  name    = "dl"
-  type    = "CNAME"
-  content   = "tc.boldman.net"
-  ttl     = 900
-  proxied = false
-}
 
 # ==========================================
 # TXT 记录 (文本/认证)
 # ==========================================
-
-resource "cloudflare_dns_record" "dnsauth_imagesearch_txt" {
-  zone_id = cloudflare_zone.boldman_net.id
-  name    = "_dnsauth.imagesearch"
-  type    = "TXT"
-  content   = "_pysas4std07f9m7wvrb0wcdffrf5xe2"
-  ttl     = 3600
-}
