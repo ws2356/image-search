89b6332509250649c7ab813afa1f9cb4be2c2569
ws2356 <wansong2356@hotmail.com>
2026-08-28 23:15:36 +0800

    feat: move web/terraform to mydevices repo

--- web/terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "zone_id" {
-  description = "Cloudflare zone ID for boldman.net"
-  value       = cloudflare_zone.boldman_net.id
-}
-
-output "nameservers" {
-  description = "Cloudflare-assigned nameservers (update your registrar)"
-  value       = cloudflare_zone.boldman_net.name_servers
-}
